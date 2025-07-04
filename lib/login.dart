16d833fbb6b39d2cb0fa95a427a1303464e51931
TajemulCSE <tajemulislamgames@gmail.com>
2025-07-04 23:57:24 +0600

    Login Design added

--- lib/login.dart
+++ lib/login.dart
@@ -4,119 +4,156 @@ import 'package:flutter/material.dart';
 import 'package:lgd/home_screen.dart';
 import 'package:lgd/sign_up.dart';
 
-class Login extends StatefulWidget{
+class Login extends StatefulWidget {
   const Login({super.key});
 
-
   @override
-  State<Login> createState()=>_Login();
+  State<Login> createState() => _Login();
 }
 
+class _Login extends State<Login> {
+  TextEditingController emailcontoller = TextEditingController();
+  TextEditingController passwordcontoller = TextEditingController();
 
-class _Login extends State<Login>{
+  String email = "", password = "";
 
-  TextEditingController emailcontoller=TextEditingController();
-  TextEditingController passwordcontoller=TextEditingController();
-
-String email="",password="";
-
-userLogin() async{
-  setState(() {
-      email=emailcontoller.text;
-      password=passwordcontoller.text;
+  userLogin() async {
+    setState(() {
+      email = emailcontoller.text;
+      password = passwordcontoller.text;
     });
 
-  if (password!="" && email!="") {
-    
-    try{
-      await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
-      Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
-      
-    } on FirebaseAuthException catch (e){
-      print("FirebaseAuthException code: ${e.code}");
+    if (password != "" && email != "") {
+      try {
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+      } on FirebaseAuthException catch (e) {
+        print("FirebaseAuthException code: ${e.code}");
 
-      if (e.code=='user-not-found') {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Email is not registered")));
-        
-      }
-      else if(e.code=='wrong-password'){
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Wrong password")));
-      }
-      else if(e.code=='invalid-credential'){
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("invalid-credential")));
+        if (e.code == 'user-not-found') {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text("Email is not registered")));
+        } else if (e.code == 'wrong-password') {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text("Wrong password")));
+        } else if (e.code == 'invalid-credential') {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text("invalid-credential")));
+        }
       }
     }
-    
   }
 
-
-
-
-}
-
-
-@override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text("LGD Login"), backgroundColor: Colors.red),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
 
-
-
-        appBar: AppBar(
-            title: Text("LGD Login"),backgroundColor: Colors.red,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 252, 144, 3),
+              const Color.fromARGB(255, 255, 199, 95),
+            ],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
         ),
-        body: 
-            Column(
-              children: [
-                Row(
-                  children: [
-                    Text("Email:")
-                    ,
-                    SizedBox(
-                      width: 10,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                    image: AssetImage(
+                      "assets/photos/LGD_NEW_LOGO_BG_Removed.png",
                     ),
-                  Expanded(child: TextField(
-                    decoration: InputDecoration(hintText: "Enter your Email address here "),
-                    controller: emailcontoller,
-                  ))
-                  ],
-                
-                ),
-                Row(
-                  children: [
-                    Text("Password:"),
-                    SizedBox(width: 10,),
-                    Expanded(child: TextField(
+                    height: 200,
+                    fit: BoxFit.contain,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Email:",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Enter your Email address here ",
+                        prefixIcon: Icon(Icons.email_outlined),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      controller: emailcontoller,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Password:",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: TextField(
                       decoration: InputDecoration(hintText: "Enter Password"),
                       controller: passwordcontoller,
-                    ))
-                  ],
-                ),
-                Row(
-                  children: [
-                    ElevatedButton(onPressed:(){
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
                       userLogin();
                       print("login button");
-                    }, child: Text("Login"))
-                  ],
-                  
-                ),
-                Row(
-                  children: [
-                    Flexible(child: Text("Not a member?")),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>SignUp()));
-                      },
-                      child: Text("Sign Up",style: TextStyle(color: Colors.blue),),
-                    )
-                  ],
-                )
-                
-              ],
-            )
-          
-        
+                    },
+                    child: Text("Login"),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Flexible(child: Text("Not a member?")),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUp()),
+                      );
+                    },
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
-  
-}
+  }
 }
