13499d34b426d32b9fc17a97e114cb1b7b0a4c6f
TajemulCSE <tajemulislamgames@gmail.com>
2025-07-04 21:50:25 +0600

    Login Succesfull

--- lib/login.dart
+++ lib/login.dart
@@ -1,8 +1,59 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:lgd/home_screen.dart';
 import 'package:lgd/sign_up.dart';
 
-class Login extends StatelessWidget{
+class Login extends StatefulWidget{
   const Login({super.key});
+
+
+  @override
+  State<Login> createState()=>_Login();
+}
+
+
+class _Login extends State<Login>{
+
+  TextEditingController emailcontoller=TextEditingController();
+  TextEditingController passwordcontoller=TextEditingController();
+
+String email="",password="";
+
+userLogin() async{
+  setState(() {
+      email=emailcontoller.text;
+      password=passwordcontoller.text;
+    });
+
+  if (password!="" && email!="") {
+    
+    try{
+      await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+      
+    } on FirebaseAuthException catch (e){
+      print("FirebaseAuthException code: ${e.code}");
+
+      if (e.code=='user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Email is not registered")));
+        
+      }
+      else if(e.code=='wrong-password'){
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Wrong password")));
+      }
+      else if(e.code=='invalid-credential'){
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("invalid-credential")));
+      }
+    }
+    
+  }
+
+
+
+
+}
+
 
 @override
   Widget build(BuildContext context) {
@@ -25,6 +76,7 @@ class Login extends StatelessWidget{
                     ),
                   Expanded(child: TextField(
                     decoration: InputDecoration(hintText: "Enter your Email address here "),
+                    controller: emailcontoller,
                   ))
                   ],
                 
@@ -35,12 +87,16 @@ class Login extends StatelessWidget{
                     SizedBox(width: 10,),
                     Expanded(child: TextField(
                       decoration: InputDecoration(hintText: "Enter Password"),
+                      controller: passwordcontoller,
                     ))
                   ],
                 ),
                 Row(
                   children: [
-                    ElevatedButton(onPressed:(){}, child: Text("Login"))
+                    ElevatedButton(onPressed:(){
+                      userLogin();
+                      print("login button");
+                    }, child: Text("Login"))
                   ],
                   
                 ),
